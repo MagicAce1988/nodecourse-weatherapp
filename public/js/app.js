57b46336494d3729fb3c10119faf5064d307edb6
Talmacel Marian - Silviu <talmacelmarian@gmail.com>
2019-06-23 01:33:33 +0100

    Removed ip location from server side file - getting it with express istead

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
-// const iplocation = require("iplocation").default;
-
-// let CountryCode =''
-
-
-console.log('Javascript is loaded')
-
-function user_location() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          const ip=JSON.parse(this.responseText)
-          const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
@@ -25,7 +13,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageTwo.textContent='Loading...'
     messageThree.textContent=''
 
-    fetch('/weather?adress='+ location + '&ipadress=' + ip.ip).then((response)=>{
+    fetch('/weather?adress='+ location).then((response)=>{
     response.json().then((data)=>{
         if (data.error){
             messageOne.textContent=data.error
@@ -41,44 +29,3 @@ weatherForm.addEventListener('submit',(e)=>{
 })
 
 })
-        
-      }
-    };
-    xhttp.open("GET", "//api.ipify.org?format=json", true);
-    xhttp.send();
-  }
-
-  user_location()
-
-// const weatherForm = document.querySelector('form')
-// const search = document.querySelector('input')
-// const messageOne = document.querySelector('#message-1')
-// const messageTwo = document.querySelector('#message-2')
-// const messageThree = document.querySelector('#message-3')
-
-// weatherForm.addEventListener('submit',(e)=>{
-//     e.preventDefault()
-
-//     const location=search.value
-
-//     messageOne.textContent=''
-//     messageTwo.textContent='Loading...'
-//     messageThree.textContent=''
-
-//     fetch('/weather?adress='+ location).then((response)=>{
-//     response.json().then((data)=>{
-//         if (data.error){
-//             messageOne.textContent=data.error
-//             messageTwo.textContent=''
-//             messageThree.textContent=''
-
-//         } else {
-//             console.log(data)
-//             messageOne.textContent=''
-//             messageTwo.textContent=data.location
-//             messageThree.textContent=data.summary + '. Pe moment sunt ' + data.degrees + ' grade celsius. Sansa de a ploua este de ' + data.chanceOfRain + ' %.'
-//         }
-//     })
-// })
-
-// })
